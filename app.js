66d50c9e26483aa882fd6096acab402d8d7f146a
sangsu1 <nss2882@naver.com>
2023-11-11 11:44:50 +0900

    나상수

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const app = express();
 const PORT = 8080;
 app.listen(PORT, function() {
     console.log('Server Init');
+    console.log('나상수');
     console.log('Listening on port: ', PORT);
 });
