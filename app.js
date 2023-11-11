f43cd0de2abefa6367484b49589e309106dd09ff
Sangwoo421 <asw421@naver.com>
2023-11-11 11:40:04 +0900

    수정

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express();
 
 const PORT = 8080;
 app.listen(PORT, function() {
-    console.log('hi');
+    console.log('Server Init');
     console.log('Listening on port: ', PORT);
 });
